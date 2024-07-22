c5caecd11c9efcc85cc12c4455827a13ccfc7f9d
Prathyusha-Mokkapati <prathyushamokkapati1998@gmail.com>
2024-07-22 15:12:49 -0700

    Create server.js

---
+++ src/itc505/lab-7/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/itc505/lab7/', (req, res) => {
+  const { Noun, pronoun, verb, adverb, adjective } = req.body;
+  const storySelect = req.body.storySelect;
+
+  let result = '';
+
+  switch (storySelect) {
+    case 'story1':
+      result = `Story 1: In a galaxy far, far away, a brave ${Noun} named ${pronoun} embarked on a quest to save the ${adjective} ${Noun}. With the help of the wise ${Noun}, ${pronoun} learned to master the ways of the Force and wield a ${adjective} lightsaber against the forces of ${adjective} evil.`;
+      break;
+    case 'story2':
+      result = `Story 2: The Rebel Alliance, led by Princess ${Noun}, fought against the oppressive ${Noun} of the ${adjective} Empire. As the rebels planned their next move, they relied on the daring pilot ${Noun} to fly a ${adjective} starship into the heart of the enemy's ${Noun}.`;
+      break;
+    case 'story3':
+      result = `Story 3: On the desert planet of ${Noun}, a scavenger named ${Noun} discovered a mysterious ${Noun} that held the key to the location of a long-lost ${adjective} Jedi temple. Joined by the loyal droid ${Noun}, ${pronoun} set out on a perilous journey to unlock the secrets of the ${adjective} Force.`;
+      break;
+    case 'story4':
+      result = `Story 4: Darth ${Noun}, a Sith Lord with a ${adjective} obsession for power, sought to crush the ${Noun} and rule the galaxy with an iron fist. In a climactic battle, the Jedi ${Noun} confronted Darth ${Noun} with a ${adjective} lightsaber, determined to bring an end to the Sith's reign of ${adjective} terror.`;
+      break;
+    case 'story5':
+      result = `Story 5: In the cantina on the bustling planet of ${Noun}, a diverse group of ${Noun} and ${Noun} gathered to celebrate their victories and share tales of their ${adjective} adventures. The atmosphere was filled with the sounds of lively music, and the air was thick with the scent of exotic ${Noun}.`;
+      break;
+    default:
+      result = 'Invalid story selection';
+      break;
+  }
+
+  res.send(result);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
